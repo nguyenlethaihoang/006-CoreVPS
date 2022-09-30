43111d1ec77bd9b7c2c7a32b840f5269d93d8eb6
nguyenlethaihoang <hoangnguyenthaile@gmail.com>
2022-09-30 21:25:40 +0700

    123

--- client/src/pages/TellerOperation/AccountManagement/CurrentAndNonTermSavingAccount/CashWithdrawal.js
+++ client/src/pages/TellerOperation/AccountManagement/CurrentAndNonTermSavingAccount/CashWithdrawal.js
@@ -1,4 +1,4 @@
-import { Accordion, AccordionDetails, AccordionSummary, Autocomplete, Button, TextField, Typography } from "@mui/material"
+import { Accordion, AccordionDetails, AccordionSummary, Autocomplete, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";    
 import { useEffect, useState } from "react";
 import Select_Custom from "../../../../components/Select_Custom";
@@ -9,7 +9,39 @@ import Popup_Custom_Fail from "../../../../components/Popup_Custom_Fail";
 import axios from "axios";
 import Notification_Custom from "../../../../components/Notification_Custom";
 
+
+const accountTypeData = [
+    {id: 1,
+    Name: 'Current & Non-Term Saving Account' },
+    {
+    id: 2,
+    Name: 'Saving Account - Arrear'},
+    {
+    id: 3,
+    Name: 'Saving Account - Periodic'},
+    {
+    id: 4,
+    Name: 'Saving Account - Discounted'},
+    {
+    id: 5,
+    Name: 'Loan Working Account'},
+]
+
+const currencyData = [{id: 1,Name: 'EUR' },{id: 2,Name: 'USD'},{id: 3,Name: 'GBP'},{id: 4,Name: 'JPY'},{id: 5,Name: 'VND'},]
+
+
+const waiveChargesData = [
+    {id: 1,
+    Name: 'YES' },
+    {
+    id: 2,
+    Name: 'NO'},
+
+]
+
 function CashWithdrawal() {
+    const [age, setAge] = useState('');
+    const [cashAccount, setCashAccount] = useState('');
     return (
         <div>
             <Accordion >
@@ -31,7 +63,142 @@ function CashWithdrawal() {
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails style={{ paddingLeft: "30px"}}>
+                <div
+                            style={{ 
+                                display: "flex", 
+                                width: "100%", 
+                                flexWrap: "wrap"
+                            }}
+                    >
+                        <Select_Custom props1="Account Type" props2="35" props3="city" props4={accountTypeData}/>
+                        <TextField 
+                            id="txtCustomerAccount" 
+                            label="Customer Account" 
+                            variant="outlined" 
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                            required
+                            onChange={() => {
+                                
+                            }}
+                        />
+                        <TextField 
+                            id="txtCurrency" 
+                            label="Currency" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtAmount" 
+                            label="Amount" 
+                            variant="outlined" 
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtNarrative" 
+                            label="Narrative" 
+                            variant="outlined" 
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                    </div>
+                    <br></br>
+                    <div
+                        style={{ 
+                            display: "flex", 
+                            width: "100%", 
+                            flexWrap: "wrap"
+                        }}
+                    >
+                        <TextField 
+                            id="txtTellerID" 
+                            label="Teller ID" 
+                            variant="outlined" 
+                            required
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                    </div>
+                    <br></br>
+                    
+                    <div
+                        style={{ 
+                            display: "flex", 
+                            width: "100%", 
+                            flexWrap: "wrap"
+                        }}
+                    >
+                        <FormControl sx={{ m: 0, minWidth: `25ch`,marginRight: "30px",marginBottom: "20px" }}>
+                            <InputLabel id="lblCurrencyPaid">Currency Paid</InputLabel>
+                            <Select
+                                labelId="lblCurrencyPaid"
+                                id="sltCurrencyPaid"
 
+                                value={age}
+                                label="Currency Paid"
+                                onChange={(event) => {
+                                    setAge(event.target.value)
+                                    setCashAccount(`${currencyData[event.target.value-1].Name} - 1001-1122-2002`)
+                                }}
+                            >
+                                {currencyData.map((data,index) => {
+                                    return (
+                                        <MenuItem key={index} value={data.id}>{data.Name}</MenuItem>
+                                    )
+                                })}
+                            </Select>
+                        </FormControl>
+                        {/* Show Account */}
+                        <TextField 
+                            id="txtCashAccount" 
+                            label="Cash Account" 
+                            variant="outlined" 
+                            value={cashAccount}
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtDealRate" 
+                            label="Deal Rate" 
+                            variant="outlined" 
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtAmountPaid" 
+                            label="Amount Paid" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtCustBal" 
+                            label="Cust Bal" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtNewCustBal" 
+                            label="New Cust Bal" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                    </div>
+                    <br></br>
+                    <div
+                        style={{ 
+                            display: "flex", 
+                            width: "100%", 
+                            flexWrap: "wrap"
+                        }}
+                    >
+                        <Select_Custom props1="Waive Charges?" props2="35" props3="city" props4={waiveChargesData}/>
+                        <TextField 
+                            id="txtPrintLnNoofPS" 
+                            label="Print LnNo of PS" 
+                            // variant="outlined" 
+                            value="1"
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                    </div>
                 </AccordionDetails>
             </Accordion>
         </div>
--- client/src/pages/TellerOperation/AccountManagement/CurrentAndNonTermSavingAccount/TransferWithdrawal.js
+++ client/src/pages/TellerOperation/AccountManagement/CurrentAndNonTermSavingAccount/TransferWithdrawal.js
@@ -8,6 +8,33 @@ import Popup_Custom from "../../../../components/Popup_Custom";
 import Popup_Custom_Fail from "../../../../components/Popup_Custom_Fail";
 import axios from "axios";
 import Notification_Custom from "../../../../components/Notification_Custom";
+import DatePicker_Custom from "../../../../components/DatePicker_Custom";
+
+const accountTypeData = [
+    {id: 1,
+    Name: 'Current & Non-Term Saving Account' },
+    {
+    id: 2,
+    Name: 'Saving Account - Arrear'},
+    {
+    id: 3,
+    Name: 'Saving Account - Periodic'},
+    {
+    id: 4,
+    Name: 'Saving Account - Discounted'},
+    {
+    id: 5,
+    Name: 'Loan Working Account'},
+]
+
+const waiveChargesData = [
+    {id: 1,
+    Name: 'YES' },
+    {
+    id: 2,
+    Name: 'NO'},
+
+]
 
 function TransferWithdrawal() {
     return (
@@ -31,7 +58,134 @@ function TransferWithdrawal() {
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails style={{ paddingLeft: "30px"}}>
-
+                
+                    <div
+                        style={{ 
+                            display: "flex", 
+                            width: "100%", 
+                            flexWrap: "wrap"
+                        }}
+                    >
+                        <h3>Debit Information</h3>
+                    </div>
+                    <div
+                        style={{ 
+                            display: "flex", 
+                            width: "100%", 
+                            flexWrap: "wrap"
+                        }}
+                    >
+                        <TextField 
+                            id="txtCustomer" 
+                            label="Customer" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <Select_Custom props1="Account Type" props2="35" props3="city" props4={accountTypeData}/>
+                        <TextField 
+                            id="txtDebitAccount" 
+                            label="Debit Account" 
+                            variant="outlined" 
+                            required
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtCurrency" 
+                            label="Currency" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtDebitAmt" 
+                            label="Debit Amt" 
+                            variant="outlined"  
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtCust Bal" 
+                            label="Cust Bal" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtNewCustBal" 
+                            label="New Cust Bal" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <DatePicker_Custom props1="Value Date" props2="30" props3="YES" />
+                    </div>
+                    <div
+                        style={{ 
+                            display: "flex", 
+                            width: "100%", 
+                            flexWrap: "wrap"
+                        }}
+                    >
+                        <h3>Credit Information</h3>
+                    </div>
+                    <div
+                        style={{ 
+                            display: "flex", 
+                            width: "100%", 
+                            flexWrap: "wrap"
+                        }}
+                    >
+                        <TextField 
+                            id="txtCustomer" 
+                            label="Customer" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtCreditAccount" 
+                            label="Credit Account" 
+                            variant="outlined" 
+                            required
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtCurrency" 
+                            label="Currency" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtDealRate" 
+                            label="Deal Rate" 
+                            variant="outlined"  
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <TextField 
+                            id="txtAmtCreditForCust" 
+                            label="Amt Credit For Cust" 
+                            variant="outlined" 
+                            disabled
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                        <DatePicker_Custom props1="Value Date" props2="30" props3="YES" />
+                    </div>
+                    <div
+                        style={{ 
+                            display: "flex", 
+                            width: "100%", 
+                            flexWrap: "wrap"
+                        }}
+                    >
+                        <Select_Custom props1="Waive Charges?" props2="35" props3="city" props4={waiveChargesData}/>
+                        <TextField 
+                            id="txtPrintLnNoofPS" 
+                            label="Print LnNo of PS" 
+                            variant="outlined" 
+                            sx={{marginRight: "30px",marginBottom: "20px" }}
+                        />
+                    </div>
                 </AccordionDetails>
             </Accordion>
         </div>
