3f4ba4328f0b8e8397c10ed5d77f1d53f49bf974
nksot21 <20520225@gm.uit.edu.vn>
2022-12-15 00:30:04 +0700

    fix

--- server/src/controllers/storage/subIndustry.js
+++ server/src/controllers/storage/subIndustry.js
@@ -1,5 +1,5 @@
 const industry = require('../../models/storage/industry')
-const subIndustryModel = require('../../models/storage/subindustry')
+const subIndustryModel = require('../../models/storage/subIndustry')
 const asyncHandler = require('../../utils/async')
 const appError = require('../../utils/appError')
 
